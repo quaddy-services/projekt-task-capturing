5af5831f3eef58e017aa76cbbe1740a460590d5f
Stefan Cordes <2789123+quaddy-services@users.noreply.github.com>
2019-02-10 10:41:01 +0100

    pull before build

--- make.bat
+++ make.bat
@@ -1,5 +1,13 @@
 echo on
+git pull
+if %errorlevel% == 1 pause
 echo %date% >src/version.txt
+git add src/version.txt
+if %errorlevel% == 1 pause
+git commit -m %date%
+if %errorlevel% == 1 pause
+git push
+if %errorlevel% == 1 pause
 call mvn clean install
 if %errorlevel% == 1 pause
 :ende
