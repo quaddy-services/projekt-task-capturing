c90639bdcac314e0bd41803cb4cf36d723cc34b5
cas <2795993+rscadrde@users.noreply.github.com>
2020-03-21 12:23:41 +0100

    Switch to munit 5 (and JDK 8)

--- tests/src/de/quaddy_services/ptc/enterprise/PtcRemoteCallTestManual.java
+++ tests/src/de/quaddy_services/ptc/enterprise/PtcRemoteCallTestManual.java
@@ -1,19 +1,23 @@
 package de.quaddy_services.ptc.enterprise;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.List;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 /**
  * Manual test. Can only run if server is running.
- * 
+ *
  * @author user
  */
-public class PtcRemoteCallTestManual extends TestCase {
+public class PtcRemoteCallTestManual {
+	@Test
+	@Disabled
 	public void testGetResource() throws Exception {
 		PtcRemoteCall tempRemoteCall = new PtcRemoteCall("localhost:334");
-		List<String> tempTasks = tempRemoteCall
-				.getBookableTaskNames("test");
+		List<String> tempTasks = tempRemoteCall.getBookableTaskNames("test");
 		System.out.println(tempTasks);
 		assertEquals(3, tempTasks.size());
 	}
